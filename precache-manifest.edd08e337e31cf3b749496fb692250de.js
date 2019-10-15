7c2e389c62dbb782a8c0dc9a1e1e9f4a05134b70
he-zhiyong <15988134176@163.com>
2019-10-15 10:50:35 +0000

    Deploying to gh-pages from master 41476dbf764a6a822a6028ee192ce7ccacc8f5e3

---
+++ precache-manifest.edd08e337e31cf3b749496fb692250de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58277a1b6583a7a3cddd1e3895a7e974",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "b55293152cf81699a34a",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5f48f2a6a531e608a10b",
+    "url": "/github-actions-demo/static/js/2.092e4bd0.chunk.js"
+  },
+  {
+    "revision": "b55293152cf81699a34a",
+    "url": "/github-actions-demo/static/js/main.d621f756.chunk.js"
+  },
+  {
+    "revision": "ba0f86e5a6948c494ff2",
+    "url": "/github-actions-demo/static/js/runtime-main.03d0611e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
